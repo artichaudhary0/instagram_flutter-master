24992d14752cf3e617241143ee20bcbeff170fc3
artichaudhary0 <chaudhary.aarti1998@gmail.com>
2024-03-19 09:55:44 +0530

    second

--- lib/screens/error_screen.dart
+++ lib/screens/error_screen.dart
@@ -1,19 +1,1 @@
 
-import 'package:flutter/material.dart';
-
-class Error2Screen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Stack(
-        fit: StackFit.expand,
-        children: [
-          Image.asset(
-            "assets/error.png",
-            fit: BoxFit.cover,
-          ),
-        ],
-      ),
-    );
-  }
-}
